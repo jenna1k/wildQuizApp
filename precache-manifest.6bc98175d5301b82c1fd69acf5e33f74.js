d1a260d7351a45a46e3bc1d12927604896b59ae6
jenna1k <jenna_kim1@hotmail.com>
2019-04-16 12:19:43 +0200

    Updates

---
+++ precache-manifest.6bc98175d5301b82c1fd69acf5e33f74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b04bbd886348a1b0c9c9",
+    "url": "/wildQuizApp/static/js/runtime~main.c1f7b38c.js"
+  },
+  {
+    "revision": "c8d6275d9a9c8aa45c45",
+    "url": "/wildQuizApp/static/js/main.cdd14b5f.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/wildQuizApp/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "c8d6275d9a9c8aa45c45",
+    "url": "/wildQuizApp/static/css/main.41fa3494.chunk.css"
+  },
+  {
+    "revision": "f665dfa0e8c362fb7a4ae6924e93fd4b",
+    "url": "/wildQuizApp/index.html"
+  }
+];
