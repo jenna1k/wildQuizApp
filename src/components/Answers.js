17150dbb07add5c6300c8cbdfa015f3a4d684a12
jenna <jenna_kim1@hotmail.com>
2019-05-07 02:16:15 +0200

    added key in list

--- src/components/Answers.js
+++ src/components/Answers.js
@@ -7,16 +7,22 @@ const Answers = ({correct_answer, incorrect_answers})=> {
   for (let i = 0; i < data.length; i++){
     list.push(
       <Col lg="6" sm="12">
-      <Button className="m-1" color="info" size="lg" block>{data[i]}</Button>
+      <Button className="m-1" color="info" size="lg" key={i} block onClick={clickButton}>{data[i]}</Button>
       </Col>
     )
   }
+
+  const clickButton = (e) => {
+    console.log(e);
+    e.preventDefault();
+  }
+
   return(
     <Card className="border-0 bg-light">
       <Row>
         {list}
         <Col lg="6" sm="12">
-        <Button className="m-1" color="info" size="lg" block>{correct_answer}</Button>
+        <Button className="m-1" color="info" size="lg" block onClick={clickButton}>{correct_answer}</Button>
         </Col>
       </Row>
     </Card>
