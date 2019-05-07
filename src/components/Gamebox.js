1cd4501f7e2fba4aaa8ca3295d50baa992fa1155
jenna <jenna_kim1@hotmail.com>
2019-05-07 02:08:36 +0200

    adjusted indent

--- src/components/Gamebox.js
+++ src/components/Gamebox.js
@@ -9,25 +9,25 @@ import ProgressBar from '../components/ProgressBar';
 const Gamebox = ({ quiz }) => {
   return(
 		<Jumbotron fluid>
-				<Row className="d-flex justify-content-center">
-						<Col sm="6">
-								<CardHeader tag="h3" className="bg-dark">
-										<CardTitle className="text-white d-flex justify-content-center">Solve quizes from 6 categories!</CardTitle>
-								</CardHeader>
-								<ProgressBar />
-								<CardBody className="bg-light">
-								<CardText>
-										<Button outline color="info" className="m-1 d-inline" disabled>{quiz.category}</Button>
-										<Button outline color="info" className="m-1 d-inline" disabled>Difficulty : {quiz.difficulty}</Button>
-								</CardText>
-								<Question question={quiz.question}/>
-								<Answers correct_answer={quiz.correct_answer} incorrect_answers={quiz.incorrect_answers}/>
-								</CardBody>
-								<CardFooter className="bg-dark">
-										<Button outline color="success" disabled>Score: 90</Button>
-								</CardFooter>
-						</Col>
-				</Row>
+      <Row className="d-flex justify-content-center">
+        <Col sm="6">
+          <CardHeader tag="h3" className="bg-dark">
+            <CardTitle className="text-white d-flex justify-content-center">Solve quizes from 6 categories!</CardTitle>
+          </CardHeader>
+          <ProgressBar />
+          <CardBody className="bg-light">
+          <CardText>
+            <Button outline color="info" className="m-1 d-inline" disabled>{quiz.category}</Button>
+            <Button outline color="info" className="m-1 d-inline" disabled>Difficulty : {quiz.difficulty}</Button>
+          </CardText>
+          <Question question={quiz.question}/>
+          <Answers correct_answer={quiz.correct_answer} incorrect_answers={quiz.incorrect_answers}/>
+          </CardBody>
+          <CardFooter className="bg-dark">
+            <Button outline color="success" disabled>Score: 90</Button>
+          </CardFooter>
+        </Col>
+      </Row>
 		</Jumbotron>
 );
 }
