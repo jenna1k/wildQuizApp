87e370bc054df9f923558268e33e77809811ad87
Anastasiya Aliashkevich <a.v.aliashkevich@gmail.com>
2019-05-11 18:13:31 +0200

    api url is placed to defaultProps, method with api requst creation added

--- src/components/game/GameSettings.js
+++ src/components/game/GameSettings.js
@@ -7,9 +7,15 @@ import './GameSettings.css';
 
 
 export default class GameSettings extends React.Component {
+  static defaultProps = {
+    url: 'https://opentdb.com/'
+  }
+
   constructor(props) {
     super(props);
 
+    this.generateAPIRequestURL = this.generateAPIRequestURL.bind(this);
+    this.getSessionToken = this.getSessionToken.bind(this);
     this.setCategoryHandler = this.setCategoryHandler.bind(this);
     this.setAmountHandler = this.setAmountHandler.bind(this);
     this.setDifficultyHandler = this.setDifficultyHandler.bind(this);
@@ -17,11 +23,12 @@ export default class GameSettings extends React.Component {
     this.state = {
       step: 'welcome', // 'welcome', 'selectCategory', 'selectDifficulty', 'selectAmount'
       type: 'multiple',
-      difficulty: 'any', //
       category: 'any', // API call: <url> + (this.state.category == "any" ? "" : ("&category=" + this.state.category))
+      difficulty: 'any', //
       amount: 10, // handle values outside 1-50 
       token: '' // Session Tokens are unique keys that will help keep track of the questions the API has already retrieved. By appending a Session Token to a API Call, the API will never give you the same question twice. 
     };
+    console.log(this.props.url);
   }
 
   componentDidMount() {
@@ -30,7 +37,7 @@ export default class GameSettings extends React.Component {
   }
 
   getSessionToken() {
-    fetch("https://opentdb.com/api_token.php?command=request")
+    fetch(this.props.url + 'api_token.php?command=request')
       .then(res => res.json())
       .then(data => { this.setState({ token: data.token }) })
       .catch(error => console.log(error));
@@ -54,8 +61,15 @@ export default class GameSettings extends React.Component {
     })
   }
 
-  generateAPIRequest() {
+  generateAPIRequestURL() {
+    var url = this.props.url
+      + "api.php?token=" + this.state.token
+      + (this.state.category === 'any' ? "" : "&category=" + this.state.category)
+      + (this.state.difficulty === 'any' ? "" : "&difficulty=" + this.state.difficulty)
+      + "&amount=" + this.state.amount
+      + "&type=" + this.state.type;
 
+    console.log(url);
   }
 
   render() {
@@ -63,10 +77,10 @@ export default class GameSettings extends React.Component {
     switch (this.state.step) {
       case 'welcome':
         customizedSetting =
-          <div>
+          <div className="text-center">
             <h1>Welcome to Wild Quiz</h1>
-            <Button>Play</Button>
-            <Button>Customize Game</Button>
+            <Button color="success" size="lg" onClick={this.generateAPIRequestURL}> Play</Button>
+            <Button color="warning" size="lg">Custom Game</Button>
           </div>
         break;
       case 'selectCategory':
