380b065c7f7d8dd73292f88cda0aff93b564748d
Anastasiya Aliashkevich <a.v.aliashkevich@gmail.com>
2019-05-15 14:50:04 +0200

    fixes for profile

--- src/components/about/Profiles.js
+++ src/components/about/Profiles.js
@@ -5,117 +5,110 @@ import {
   CardImg,
   CardTitle,
   CardText,
-  CardDeck,
   CardSubtitle,
   CardBody,
-  Jumbotron
+  Container,
+  Col,
+  Row
 } from 'reactstrap';
 
 const Profiles = props => {
   return (
-    <Jumbotron id="profile">
-      <CardDeck>
-        <Card>
-          <CardImg
-            top
-            src="https://ca.slack-edge.com/T6SG2QGG2-UHRJLPC0L-ebab15075c82-512"
-            alt="Profile Picture"
-            className="rounded-circle"
-          />
+    <Container id="profile">
+      <Row>
+        <Col lg="4">
+          <Card body>
+            <CardImg
+              top
+              src="https://ca.slack-edge.com/T6SG2QGG2-UHRJLPC0L-ebab15075c82-512"
+              alt="Profile Picture"
+              className="rounded-circle"
+            />
+            <CardBody>
+              <CardTitle>Dajung Kim</CardTitle>
+              <CardSubtitle><em>former accountant</em></CardSubtitle>
+              <CardText className="description">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content.This content is a little bit nkjgkjg
+                longer vmvvmmmvmnvmmnvmvmn.
+              </CardText>
+              <CardText className="social">
+                <a href="https://github.com/jenna1k" target="_blank" rel="noopener noreferrer" >
+                  <i className="fab fa-github" />
+                </a>
+                <a href="https://twitter.com/JennaDJK" target="_blank" rel="noopener noreferrer" >
+                  <i className="fab fa-twitter-square" />
+                </a>
+                <a href="https://www.linkedin.com/in/kim-jenna-4292b0132/" target="_blank" rel="noopener noreferrer" >
+                  <i className="fab fa-linkedin" />
+                </a>
+              </CardText>
+            </CardBody>
+          </Card>
+        </Col>
 
-          <CardBody>
-            <CardTitle tag="h2">
-              <strong>Dajung Kim</strong>
-            </CardTitle>
-            <CardSubtitle tag="h4">
-              <em>former accountant</em>
-            </CardSubtitle>
+        <Col lg="4">
+          <Card body>
+            <CardImg
+              top
+              src="https://ca.slack-edge.com/T6SG2QGG2-UHS6APYKF-859fb9d29181-512"
+              alt="Profile Picture"
+              className="rounded-circle"
+            />
+            <CardBody>
+              <CardTitle>Anastasiya Aliashkevich</CardTitle>
+              <CardSubtitle><em>former QA</em></CardSubtitle>
+              <CardText className="description">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content.This content is a little bit nkjgkjg
+                longer vmvvmmmvmnvmmnvmvmn.
+              </CardText>
+              <CardText className="social">
+                <a href="https://github.com/aliashkevich" target="_blank" rel="noopener noreferrer" >
+                  <i className="fab fa-github" />
+                </a>
+                <a href="https://github.com/jenna1k" target="_blank" rel="noopener noreferrer" >
+                  <i className="fab fa-twitter-square" />
+                </a>
+                <a href="https://www.linkedin.com/in/anastasiya-aliashkevich-941091a0/" target="_blank" rel="noopener noreferrer" >
+                  <i className="fab fa-linkedin" />
+                </a>
+              </CardText>
+            </CardBody>
+          </Card>
+        </Col>
 
-            <CardText>
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content.This content is a little bit nkjgkjg
-              longer vmvvmmmvmnvmmnvmvmn.
-            </CardText>
-            <CardText className="social">
-              <a href="https://github.com/jenna1k">
-                <i className="fab fa-github" />
-              </a>
-              <a href="https://github.com/jenna1k">
-                <i className="fab fa-github" />
-              </a>
-              <a href="https://github.com/jenna1k">
-                <i className="fab fa-github" />
-              </a>
-            </CardText>
-          </CardBody>
-        </Card>
-        <Card>
-          <CardImg
-            top
-            src="https://ca.slack-edge.com/T6SG2QGG2-UHS6APYKF-859fb9d29181-512"
-            alt="Profile Picture"
-            className="rounded-circle"
-          />
-          <CardBody>
-            <CardTitle tag="h2">
-              <strong>Anastasiya Aliashkevich</strong>
-            </CardTitle>
-            <CardSubtitle tag="h4">
-              <em>former QA</em>
-            </CardSubtitle>
-
-            <CardText>
-              This card has supporting text below as a natural lead-in to
-              additional content.n,vgjkgkgkjgkjgkjgkjgkgkjgkjgkjgkjggkjgjgj.
-            </CardText>
-            <CardText className="social">
-              <a href="https://github.com/jenna1k">
-                <i className="fab fa-github" />
-              </a>
-              <a href="https://github.com/jenna1k">
-                <i className="fab fa-github" />
-              </a>
-              <a href="https://github.com/jenna1k">
-                <i className="fab fa-github" />
-              </a>
-            </CardText>
-          </CardBody>
-        </Card>
-        <Card>
-          <CardImg
-            top
-            src="https://ca.slack-edge.com/T6SG2QGG2-UHTT5TD2T-5bf3a1b22743-512"
-            alt="Profile Picture"
-            className="rounded-circle"
-          />
-          <CardBody>
-            <CardTitle tag="h2">
-              <strong>Bernadetta Nycz</strong>
-            </CardTitle>
-            <CardSubtitle tag="h4">
-              <em>former stylist</em>
-            </CardSubtitle>
-
-            <CardText>
-              {" "}
-              This card has even longer content than the first to show that
-              equal height action.
-            </CardText>
-            <CardText className="social">
-              <a href="https://github.com/jenna1k">
-                <i className="fab fa-github" />
-              </a>
-              <a href="https://github.com/jenna1k">
-                <i className="fab fa-twitter-square"></i>
-              </a>
-              <a href="https://github.com/jenna1k">
-                <i className="fab fa-linkedin"></i>
-              </a>
-            </CardText>
-          </CardBody>
-        </Card>
-      </CardDeck>
-    </Jumbotron>
+        <Col lg="4">
+          <Card body>
+            <CardImg
+              top
+              src="https://ca.slack-edge.com/T6SG2QGG2-UHTT5TD2T-5bf3a1b22743-512"
+              alt="Profile Picture"
+              className="rounded-circle"
+            />
+            <CardBody>
+              <CardTitle>Bernadetta Nycz</CardTitle>
+              <CardSubtitle><em>former stylist</em></CardSubtitle>
+              <CardText className="description">
+                This card has even longer content than the first to show that
+                equal height action.
+              </CardText>
+              <CardText className="social">
+                <a href="https://github.com/jenna1k" target="_blank" rel="noopener noreferrer" >
+                  <i className="fab fa-github" />
+                </a>
+                <a href="https://github.com/jenna1k" target="_blank" rel="noopener noreferrer" >
+                  <i className="fab fa-twitter-square"></i>
+                </a>
+                <a href="https://github.com/jenna1k" target="_blank" rel="noopener noreferrer" >
+                  <i className="fab fa-linkedin"></i>
+                </a>
+              </CardText>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
--- src/components/global/Footer.js
+++ src/components/global/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css';
 
 const Footer = () => (
   <footer className="footer">
-    <span>Made with <i className="fas fa-heart"></i> & <i className="fas fa-coffee"></i> by <a href="https://odyssey.wildcodeschool.com/?locale=en" className="link color-inherit hover-white">Wild</a> girls</span>
+    <span>Made with <i className="fas fa-heart"></i> & <i className="fas fa-coffee"></i> by <a href="https://odyssey.wildcodeschool.com/?locale=en" target="_blank" rel="noopener noreferrer" className="link color-inherit hover-white">Wild</a> girls</span>
   </footer>
 );
 
