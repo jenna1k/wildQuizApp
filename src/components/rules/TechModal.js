58eac75c53922f4a3ea980e763d396cd0b8180a2
jenna <jenna_kim1@hotmail.com>
2019-05-18 23:35:42 +0200

    replace wireframe picture

--- src/components/rules/TechModal.js
+++ src/components/rules/TechModal.js
@@ -29,54 +29,47 @@ export default class TechModal extends React.Component {
               <Row>
                 <div className="img_wrapper">
                   <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Adobe_XD_CC_icon.svg/492px-Adobe_XD_CC_icon.svg.png" alt="adobe xd" />
+                  <p className="_tech">Adobe XD</p>
                 </div>
               </Row>
               <hr />
               <Row>
-                <div className="img_wrapper">
-                  <img src="https://cdn.svgporn.com/logos/html-5.svg" alt="html5" />
-                </div>
-                <div className="img_wrapper">
-                  <img src="https://cdn.svgporn.com/logos/css-3.svg" alt="css3" />
-                </div>
-                <div className="img_wrapper">
-                  <img src="https://cdn.svgporn.com/logos/javascript.svg" alt="javascript" />
-                </div>
+                  <div className="img_wrapper">
+                    <img src="https://cdn.svgporn.com/logos/html-5.svg" alt="html5" />
+                    <img src="https://cdn.svgporn.com/logos/css-3.svg" alt="css3" />
+                    <img src="https://cdn.svgporn.com/logos/javascript.svg" alt="javascript" />
+                    <img src="https://cdn.svgporn.com/logos/react.svg" alt="react" />
+                  </div>
               </Row>
-              <hr />
               <Row>
-                <div className="img_wrapper">
-                  <img src="https://cdn.svgporn.com/logos/react.svg" alt="react" />
-                </div>
+                <p className="_tech">HTML5 / CSS3 / javascript / react.js</p>
               </Row>
               <hr />
               <Row>
                 <div className="img_wrapper">
                   <img src="https://cdn.svgporn.com/logos/bootstrap.svg" alt="bootstrap" />
-                </div>
-                <div className="img_wrapper">
                   <img src="https://madewithnetwork.ams3.cdn.digitaloceanspaces.com//spatie-space-production/1901/1de62f0c-ee9f-11e5-95c0-c515fee8e918.png" alt="reactstrap" />
                 </div>
+              </Row>
+              <Row>
+                <p className="_tech">bootstrap / reactstrap</p>
               </Row>
               <hr />
               <Row>
                 <div className="img_wrapper">
                   <img src="https://cdn.svgporn.com/logos/npm.svg" alt="npm" />
-                </div>
-              </Row>
-              <hr />
-              <Row>
-                <div className="img_wrapper">
                   <img src="https://cdn.svgporn.com/logos/git.svg" alt="git" />
-                </div>
-                <div className="img_wrapper">
                   <img src="https://cdn.svgporn.com/logos/github-icon.svg" alt="github" />
                 </div>
+              </Row>
+              <Row>
+                <p className="_tech">npm / git / github</p>
               </Row>
               <hr />
               <Row>
                 <div className="img_wrapper">
                   <img src="https://cdn.svgporn.com/logos/netlify.svg" alt="netlify" />
+                  <p className="_tech">netlify</p>
                 </div>
               </Row>
             </div>
