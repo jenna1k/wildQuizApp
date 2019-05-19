6d47d2113f6d74f3193d4801cae0158f29fbf1fe
Anastasiya Aliashkevich <a.v.aliashkevich@gmail.com>
2019-05-19 22:01:07 +0200

    layout fixed in About page

--- src/components/about/Profiles.js
+++ src/components/about/Profiles.js
@@ -17,7 +17,7 @@ function Profiles() {
     <Container id="profile">
       <Row id="profiles">
         <Col lg="4">
-          <Card body>
+          <Card>
             <CardImg
               top
               src="https://ca.slack-edge.com/T6SG2QGG2-UHRJLPC0L-ebab15075c82-512"
@@ -27,13 +27,13 @@ function Profiles() {
             <CardBody>
               <CardTitle><p>Dajung Kim</p></CardTitle>
               <CardSubtitle><em>former accountant</em></CardSubtitle>
-              <div className="description">
-                <CardText className="description">
-                  I'm Jenna from South Korea. Traveled Singapore, Australia and now I'm here in Germany. I like to travel & coding!
+              <div className="card-content">
+                <div className="description">
+                  <CardText className="description">
+                    I'm Jenna from South Korea. Traveled Singapore, Australia and now I'm here in Germany. I like to travel & coding!
                 </CardText>
-              </div>
-              <div className="social">
-                <CardText className="social">
+                </div>
+                <div className="social">
                   <a href="https://github.com/jenna1k" target="_blank" rel="noopener noreferrer" >
                     <i className="fab fa-github" />
                   </a>
@@ -43,14 +43,14 @@ function Profiles() {
                   <a href="https://www.linkedin.com/in/kim-jenna-4292b0132/" target="_blank" rel="noopener noreferrer" >
                     <i className="fab fa-linkedin" />
                   </a>
-                </CardText>
+                </div>
               </div>
             </CardBody>
           </Card>
         </Col>
 
         <Col lg="4">
-          <Card body>
+          <Card>
             <CardImg
               top
               src="https://ca.slack-edge.com/T6SG2QGG2-UHS6APYKF-859fb9d29181-512"
@@ -60,13 +60,13 @@ function Profiles() {
             <CardBody>
               <CardTitle>Anastasiya Aliashkevich</CardTitle>
               <CardSubtitle><em>former QA</em></CardSubtitle>
-              <div className="description">
-                <CardText className="description">
-                  My name is Anastasiya and I'm from Belarus - a country with economy and national idea being potato. I like to solve puzzles, code stuff and also I love computer games.
+              <div className="card-content">
+                <div className="description">
+                  <CardText className="description">
+                    My name is Anastasiya and I'm from Belarus - a country with economy and national idea being potato. I like to solve puzzles, code stuff and also I love computer games.
                 </CardText>
-              </div>
-              <div className="social">
-                <CardText className="social">
+                </div>
+                <div className="social">
                   <a href="https://github.com/aliashkevich" target="_blank" rel="noopener noreferrer" >
                     <i className="fab fa-github" />
                   </a>
@@ -76,14 +76,14 @@ function Profiles() {
                   <a href="https://www.linkedin.com/in/anastasiya-aliashkevich-941091a0/" target="_blank" rel="noopener noreferrer" >
                     <i className="fab fa-linkedin" />
                   </a>
-                </CardText>
+                </div>
               </div>
             </CardBody>
           </Card>
         </Col>
 
         <Col lg="4">
-          <Card body>
+          <Card>
             <CardImg
               top
               src="https://ca.slack-edge.com/T6SG2QGG2-UHTT5TD2T-5bf3a1b22743-512"
@@ -93,13 +93,13 @@ function Profiles() {
             <CardBody>
               <CardTitle>Bernadetta Nycz</CardTitle>
               <CardSubtitle><em>former stylist</em></CardSubtitle>
-              <div className="description">
-                <CardText className="description">
-                  I’m Bernadetta, currently on the journey to translate my love for art, fashion and tech into digital reality. I enjoy sun, skateboarding and expresso.
+              <div className="card-content">
+                <div className="description">
+                  <CardText className="description">
+                    I’m Bernadetta, currently on the journey to translate my love for art, fashion and tech into digital reality. I enjoy sun, skateboarding and espresso.
                 </CardText>
-              </div>
-              <div className="social">
-                <CardText className="social">
+                </div>
+                <div className="social">
                   <a href="https://github.com/Hiiiij" target="_blank" rel="noopener noreferrer" >
                     <i className="fab fa-github" />
                   </a>
@@ -109,7 +109,7 @@ function Profiles() {
                   <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer" >
                     <i className="fab fa-linkedin"></i>
                   </a>
-                </CardText>
+                </div>
               </div>
             </CardBody>
           </Card>
